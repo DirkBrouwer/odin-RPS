35efbe2b7e60a92a1d89c1c47212fc8e82c4b9b2
Dirk Brouwer <5168280+DirkBrouwer@users.noreply.github.com>
2024-10-01 11:42:59 +0200

    Remove pseudocode

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-/*
-PSEUDOCODE
-
-Step 1: Write the logic to get the computer choice
-
-1. Generate a random number
-
-2. Limit random number to three different values: 1, 2 or 3
-
-3. Return either "rock", "paper" or "scissors", depending on the random number
-*/
-
 function getComputerChoice() {
   const randomNumber = Math.ceil(Math.random() * 3);
 
@@ -22,18 +10,6 @@ function getComputerChoice() {
       return "scissors";
   }
 }
-
-/*
-PSEUDOCODE
-
-Step 2: Write the logic to get the human choice
-
-1. Prompt the user to enter one of three options: "rock", "paper" or "scissors"
-
-2. If user input is not a valid choice, inform user about invalid input and prompt user again until valid choice is entered.
-
-3. Return human choice
-*/
 
 function getHumanChoice() {
   let inputValid = false;
@@ -53,25 +29,7 @@ function getHumanChoice() {
   } while (!inputValid);
 }
 
-/*
-PSEUDOCODE
-
-Step 5: Write the logic to play the entire game
-
-1. Play 5 rounds of rock, paper, scissors
-2. After each round, add one to either the computer score or the human score, depending on the round winner.
-3. After all rounds have been played, compare both scores to each other. Declare the one with the highest score winner of the entire game
-*/
-
 function playGame() {
-  /*
-PSEUDOCODE
-
-Step 3: Declare the players score variables
-
-1. Set human score to zero
-2. Set computer score to zero
-*/
   let humanScore = 0;
   let computerScore = 0;
 
@@ -105,16 +63,6 @@ Step 3: Declare the players score variables
       console.log("It's a draw!");
     }
   }
-
-  /*
-PSEUDOCODE
-
-Step 4: Write the logic to play a single round
-
-1. Compare computer choice with human choice
-2. If one beats the other, declare the round winner. If computer choice is equal to human choice, show message saying it's a draw
-3. If computer wins, add one to computer score. If human wins, add one to human score. If it's a draw, don't add anything to either scores
-*/
 
   function playRound(humanChoice, computerChoice) {
     const capitalizedHumanChoice =
